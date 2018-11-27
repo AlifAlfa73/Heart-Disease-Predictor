1a4ddd9a64bb1f9f6db7d8de4e350dc0fc7a5ec3
Jonathan <jonalvarot@gmail.com>
2018-11-27 19:57:46 +0700

    Reactivated clustering, added oversampling with SMOTE algorithm, changed model to KNN

--- predict.py
+++ predict.py
@@ -1,9 +1,10 @@
 import pickle
 from sklearn.preprocessing import MinMaxScaler
+from numpy import array
 
 
 def read_pickle():
-    with open("data/mlp.pkl", "rb") as f:
+    with open("data/knn.pkl", "rb") as f:
         model = pickle.load(f)
     with open("data/x_test.pkl", "rb") as f:
         x_test = pickle.load(f)
@@ -13,10 +14,11 @@ def read_pickle():
 def pre_process(user_input, x_test):
     removed_input = remove_columns(user_input)
     x_test.append(removed_input)
-    scaler_test = MinMaxScaler(feature_range=(0, 1))
-    scaled_total = scaler_test.fit_transform(x_test)
-    scaled_input = scaled_total[-1]
-    return scaled_input
+    scaler = pickle.load(open('data/scaler.pkl', 'rb'))
+    removed_input = array(removed_input)
+    removed_input = removed_input.reshape(1, -1)
+    scaled_input = scaler.transform(removed_input)
+    return scaled_input[0]
 
 
 def remove_columns(user_input):
